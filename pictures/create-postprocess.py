ab3941aa2b3981f9e55e70b87c2cb3ddb601e6a2
Alfred Tuffreau <tuffreau.alfred@gmail.com>
2020-04-18 16:27:24 +0200

    fix print

--- pictures/create-postprocess.py
+++ pictures/create-postprocess.py
@@ -2,5 +2,4 @@ def main(event, context):
   for record in event["Records"]:
     filename = record["s3"]["object"]["key"]
     filesize = record["s3"]["object"]["size"]
-    print('New object has been created: %(filename)s (%(filesize)d bytes)' %
-          { "filename": filename, "filesize": filesize})
+    print('New object has been created: ', filename, ' (', filesize, ' bytes)')
--- pictures/remove-postprocess.py
+++ pictures/remove-postprocess.py
@@ -2,5 +2,4 @@ def main(event, context):
   for record in event["Records"]:
     filename = record["s3"]["object"]["key"]
     filesize = record["s3"]["object"]["size"]
-    print('An object has been deleted: %(filename)s (%(filesize)d bytes)' %
-          { "filename": filename, "filesize": filesize})
+    print('An object has been deleted: ', filename, ' (', filesize, ' bytes)')
