f1dd7113759fdfd218feaf41e73bc129ebb2ea3e
Alfred Tuffreau <tuffreau.alfred@gmail.com>
2020-04-18 16:31:19 +0200

    test python code

--- pictures/remove-postprocess.py
+++ pictures/remove-postprocess.py
@@ -1,5 +1,6 @@
-def main(event, context):
-  for record in event["Records"]:
-    filename = record["s3"]["object"]["key"]
-    filesize = record["s3"]["object"]["size"]
-    print('An object has been deleted: ', filename, ' (', filesize, ' bytes)')
+def main(event):
+  print(event)
+  # for record in event["Records"]:
+  #   filename = record["s3"]["object"]["key"]
+  #   filesize = record["s3"]["object"]["size"]
+  #   print('An object has been deleted: ', filename, ' (', filesize, ' bytes)')
